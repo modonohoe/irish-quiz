be4cd79509d3c3ff351128945494ee3bc173c2ac
MEGAN ODONOHOE <MEGANODT@gmail.com>
2023-07-08 23:01:08 +0000

    Define global variables and starting functions on script.js

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//Global variables and constants
+const question = document.getElementById('question');
+const option = Array.from(document.getElementsByClassName('option-text'));
+const progressText = document.getElementById('progressText');
+const score = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+
+
+//Functions
+function runGame() {
+
+}
+
+function displayQuestion() [
+
+]
+
+function checkAnswer() {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+//Event listeners
